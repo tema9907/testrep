2d3752741b577b38a7edf108350b51fd239ecc45
tema9907 <kanal9907@gmail.com>
2022-01-27 23:00:24 +0600

    почти работает

--- cat.py
+++ cat.py
@@ -1,24 +1,41 @@
+import random
+import randmove
 from turtle import *
 setup(1000, 1000)
 Screen()
 title("Turtle Keys")
-sec  = Turtle("turtle")
+sec  = Turtle("circle")
 sec.showturtle()
+sec.penup()
+sec.setx(100)
+sec.sety(100)
+#sec.goto(random.randint(0,900),random.randint(0,900))
 move = Turtle()
 move.penup()
 #showturtle()
 def k1():
     move.forward(45)
+    randmove.randmov(sec)
+    if sec.position() == move.position():
+        print("словил")
 def k2():
     move.left(45)
+    randmove.randmov(sec)
+    if sec.position() == move.position():
+        print("словил")
 def k3():
     move.right(45)
+    randmove.randmov(sec)
+    if sec.position() == move.position():
+        print("словил")
 def k4():
     move.back(45)
-onkey(k1, "Up")
+    randmove.randmov(sec)
+    if sec.position() == move.position():
+        print("словил")
+onkey(k1, "Up")   
 onkey(k2, "Left")
 onkey(k3, "Right")
 onkey(k4, "Down")
 listen()
 mainloop()
-print('ssf')
---
+++ randmove.py
@@ -0,0 +1,14 @@
+import random
+from turtle import Turtle
+
+
+def randmov(a:Turtle):
+    r=random.randint(1, 4) 
+    if r==1:
+        return(a.forward(45))
+    if r==2:
+       return(a.left(45))
+    if r==3:
+        return(a.right(45))
+    if r==4:
+        return(a.back(45))
