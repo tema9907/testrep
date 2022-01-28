bfd1a88b2592282da900a15fd7ca2cb27923504c
NekoNikon <nikon9nikon@gmail.com>
2022-01-28 17:06:44 +0600

    collision

--- cat.py
+++ cat.py
@@ -9,47 +9,69 @@ from turtle import *
 setup(1000, 1000)
 Screen()
 title("Turtle Keys")
-sec  = Turtle("circle")
-sec.showturtle()
-sec.penup()
-sec.setx(100)
-sec.sety(100)
+mouse = Turtle("circle")
+mouse.showturtle()
+mouse.penup()
+mouse.setx(100)
+mouse.sety(100)
 
 
-# movement cat
-# def move_cat():
-
+def isCollision(cat, mouse):
+    cxcor = cat.xcor()
+    cycor = cat.ycor()
+    mxcor = mouse.xcor()
+    mycor = mouse.ycor()
+    print("cat(%i,%i) mouse(%i,%i)" % (cxcor, cycor, mxcor, mycor))
+    if ((cxcor >= mxcor-20) and (cxcor <= mxcor+20) and (cycor >= mycor-20) and (cycor <= mycor+20)):
+        return True
+    else:
+        return False
 
 # random move mouse
+
+
 def move_mouse():
     for i in range(10000):
         time.sleep(0.5)
-        randmove.randmov(sec)
+        randmove.randmov(mouse)
+
 
 thread = Thread(target=move_mouse)
 thread.start()
 
 
-#sec.goto(random.randint(0,900),random.randint(0,900))
-move = Turtle()
-move.penup()
-#showturtle()
+# cat.goto(random.randint(0,900),random.randint(0,900))
+cat = Turtle()
+cat.penup()
+# showturtle()
+
+
 def k1():
-    move.forward(45)
-    if sec.position() == move.position():
+    cat.forward(45)
+    if isCollision(cat, mouse):
         print("словил")
+
+
 def k2():
-    move.left(45)
-    if sec.position() == move.position():
+    cat.left(45)
+    # cat.shapetransform(1.0, 1.0, 0, 0)
+    if isCollision(cat, mouse):
         print("словил")
+
+
 def k3():
-    move.right(45)
-    if sec.position() == move.position():
+    cat.right(45)
+    # cat.shapetransform(0, 0, 1.0, 1.0)
+    if isCollision(cat, mouse):
         print("словил")
+
+
 def k4():
-    move.back(45)
-    if sec.position() == move.position():
+    cat.back(45)
+    if isCollision(cat, mouse):
         print("словил")
+
+
 onkey(k1, "Up")
 onkey(k2, "Left")
 onkey(k3, "Right")
