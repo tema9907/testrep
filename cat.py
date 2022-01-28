9059f1f30702e48720d45868bd423367010d7b3f
NekoNikon <nikon9nikon@gmail.com>
2022-01-28 15:42:55 +0600

    random move mouse

--- cat.py
+++ cat.py
@@ -1,6 +1,9 @@
 import random
 import randmove
+import time
+from threading import Thread
 from turtle import *
+
 setup(1000, 1000)
 Screen()
 title("Turtle Keys")
@@ -9,33 +12,46 @@ sec.showturtle()
 sec.penup()
 sec.setx(100)
 sec.sety(100)
+
+
+# movement cat
+def move_cat():
+
+
+# random move mouse
+def move_mouse():
+    for i in range(10000):
+        time.sleep(1)
+        randmove.randmov(sec)
+
+thread = Thread(target=move_mouse)
+thread.start()
+
+
 #sec.goto(random.randint(0,900),random.randint(0,900))
 move = Turtle()
 move.penup()
 #showturtle()
 def k1():
     move.forward(45)
-    randmove.randmov(sec)
     if sec.position() == move.position():
         print("словил")
 def k2():
     move.left(45)
-    randmove.randmov(sec)
     if sec.position() == move.position():
         print("словил")
 def k3():
     move.right(45)
-    randmove.randmov(sec)
     if sec.position() == move.position():
         print("словил")
 def k4():
     move.back(45)
-    randmove.randmov(sec)
     if sec.position() == move.position():
         print("словил")
-onkey(k1, "Up")   
+onkey(k1, "Up")
 onkey(k2, "Left")
 onkey(k3, "Right")
 onkey(k4, "Down")
 listen()
 mainloop()
+thread.join()
