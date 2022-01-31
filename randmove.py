e293d011530d9989c7291756c8e7424d700ef7d2
tema9907 <kanal9907@gmail.com>
2022-01-31 16:13:31 +0600

    вот теперь как человек точно

--- randmove.py
+++ randmove.py
@@ -5,10 +5,11 @@ from turtle import Turtle
 def randmov(a:Turtle):
     r=random.randint(1, 4) 
     if r==1:
+        
         return(a.forward(45))
     if r==2:
-       return(a.left(45))
+       return(a.left(90))
     if r==3:
-        return(a.right(45))
+        return(a.right(90))
     if r==4:
         return(a.back(45))
