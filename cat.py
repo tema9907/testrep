d84b3802fe206fb6b36b9ef65d47566129c04203
tema9907 <kanal9907@gmail.com>
2022-01-31 15:16:33 +0600

    newcat

--- cat.py
+++ cat.py
@@ -3,53 +3,98 @@ import randmove
 import time
 from threading import Thread
 from turtle import *
-image ='cat.gif'
-setup(1000, 1000)
 
+# for artem
+
+setup(1000, 1000)
+Screen()
 scre=Screen()
-scre.addshape(image)
+
 scre.addshape("cat_left.gif")
+scre.addshape("cat_right.gif")
+scre.addshape("cat_top.gif")
+scre.addshape("cat_down.gif")
 title("Turtle Keys")
-sec  = Turtle("circle")
-sec.showturtle()
-sec.penup()
-sec.setx(100)
-sec.sety(100)
+mouse = Turtle("circle")
+mouse.showturtle()
+mouse.penup()
+mouse.setx(100)
+mouse.sety(100)
+
+
+def isCollision(cat, mouse):
+    cxcor = cat.xcor()
+    cycor = cat.ycor()
+    mxcor = mouse.xcor()
+    mycor = mouse.ycor()
+    print("cat(%i,%i) mouse(%i,%i)" % (cxcor, cycor, mxcor, mycor))
+    if ((cxcor >= mxcor-20) and (cxcor <= mxcor+20) and (cycor >= mycor-20) and (cycor <= mycor+20)):
+        return True
+    else:
+        return False
+
+
+
+
 def move_mouse():
     for i in range(10000):
         time.sleep(0.5)
-        randmove.randmov(sec)
+        randmove.randmov(mouse)
+
 
 thread = Thread(target=move_mouse)
 thread.start()
 
-move = Turtle()
-move.penup()
-move.shape(image)
+
+
+cat = Turtle()
+cat.penup()
+
 
 
 def k1():
-    move.forward(45)
+    cat.forward(45)
+    if isCollision(cat, mouse):
+        print("словил")
 
-    if sec.position() == move.position():
-        print("словил")
+
 def k2():
-    move.left(90)
-    move.shape("cat_left.gif")
-    print(move.heading())
-    if int(move.heading())== 90:
-        move.shape("cat_left.gif")
-    if sec.position() == move.position():
+    cat.left(90)
+    
+    
+    print(cat.heading())
+    if int(cat.heading())== 90:
+        cat.shape("cat_top.gif")
+    if int(cat.heading())== 180:
+        cat.shape("cat_left.gif")
+    if int(cat.heading())== 270:
+        cat.shape("cat_down.gif")
+    if int(cat.heading())== 0:
+        cat.shape("cat_right.gif")
+    if isCollision(cat, mouse):
         print("словил")
+
+
 def k3():
-    move.right(90)
-    print("povorot"+move.heading())
-    if sec.position() == move.position():
+    cat.right(90)
+    if int(cat.heading())== 90:
+        cat.shape("cat_top.gif")
+    if int(cat.heading())== 180:
+        cat.shape("cat_left.gif")
+    if int(cat.heading())== 270:
+        cat.shape("cat_down.gif")
+    if int(cat.heading())== 0:
+        cat.shape("cat_right.gif")
+    if isCollision(cat, mouse):
         print("словил")
+
+
 def k4():
-    move.back(45)
-    if sec.position() == move.position():
+    cat.back(45)
+    if isCollision(cat, mouse):
         print("словил")
+
+
 onkey(k1, "Up")
 onkey(k2, "Left")
 onkey(k3, "Right")
